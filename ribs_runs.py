9ecce5f70d0eb7d1a02bfb548b0c2efaf44db513
ZiaZ <zia.zafari@gmail.com>
2018-11-26 23:26:32 +0000

    after temp simulation

--- ribs_runs.py
+++ ribs_runs.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import run_ideal_brittle_solid as ribs
 import params
 import numpy as np
@@ -20,10 +23,12 @@ for delta in deltas:
     temps = [-50,0,10,25,35,45,65,85,100,130]
     temps_kelvin = [ el+273 for el in temps]
     params.delta = delta
-    params.desc = ' (T='+str()+') (Δ='+str()+')'
     for temp in temps_kelvin:
+        if delta == 1.55 and temp < (65+273):
+            continue
+        params.desc = '(T='+str(temp-273)+') (Δ='+str(delta)+')'
         params.T = temp
-    ribs.ribs(params, frame_count = 1200)
+        ribs.ribs(params, frame_count = 1200)
 #-----------------Temperature Section Sim---------------
     
 end_time = time.time()
@@ -31,5 +36,5 @@ time_diff = (end_time - start_time)
 time_unit = "seconds"
 if time_diff > 60:
     time_diff /= 60
-    time_uni = 'minutes'
+    time_unit = 'minutes'
 print("the simulation took |%s %s| in total." %(time_diff, time_unit))         
